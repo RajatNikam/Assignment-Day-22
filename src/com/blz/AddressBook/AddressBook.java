2b1fbf6764cb349e5f79f9eb8e448c849ac4a92e
RajatNikam <86477706+RajatNikam@users.noreply.github.com>
2021-09-19 16:37:57 +0530

    Added method to search a person contact by city

---
+++ src/com/blz/AddressBook/AddressBook.java
@@ -0,0 +1,27 @@
+package com.blz.Assignment22;
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+        AddressBookOperations operations = new AddressBookOperations();
+        UserInputUtil input = new UserInputUtil();
+        int a = 0;
+        System.out.println("1. add\n2. display\n3. edit Contact\n4.delete \n0.exit");
+        while (a == 0) {
+            System.out.print("\nEnter your choice : ");
+
+            switch (input.intInput()) {
+                case 1 -> operations.add();
+                case 2 -> operations.display();
+                case 3 -> operations.edit();
+                case 4 -> operations.delete();
+                case 5 -> operations.searchByCity();
+                case 0 -> {
+                    a = 1;
+                    System.out.println("Thank you for your Time");
+                }
+                default -> System.out.println("Choose only from options");
+            }
+        }
+    }
+}
