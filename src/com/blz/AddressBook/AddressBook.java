173b65b2366d121c04911cf9026fed1b0e018bdc
RajatNikam <86477706+RajatNikam@users.noreply.github.com>
2021-09-19 15:24:51 +0530

    Added method to add new Contact

---
+++ src/com/blz/AddressBook/AddressBook.java
@@ -0,0 +1,19 @@
+package com.blz.Assignment22;
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+        AddressBookOperations operations = new AddressBookOperations();
+        UserInputUtil input = new UserInputUtil();
+
+        while (true) {
+            System.out.println("1. add\n2. display");
+            if (input.intInput() == 1) {
+                operations.add();
+            }
+            else {
+                operations.display();break;
+            }
+        }
+    }
+}
